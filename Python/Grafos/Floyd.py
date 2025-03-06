c1e9054933634f448f2d2e5db2d59e12e53b14dd
DesignMA <designsma.contact@gmail.com>
2025-03-06 08:41:05 -0600

    "Finalizando el algoritmo de floyd, esperando pruebas, subiendo algoritmo de dijkstra corregido."

--- Python/Grafos/Floyd.py
+++ Python/Grafos/Floyd.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt # usado para graficar los nodos
 import numpy as np
 G = nx.Graph() # instanciando grafo no dirigido
-print("Defina los vertices del grafo\n")
+print("Defina los vertices del grafo no dirigido\n")
 while True:
     vertice = input("\nIntroduce un vertice\n-1 para salir : ")
     
