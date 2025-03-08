f2b8ba4c47fb741288f25cc2f08224a25a785c17
DesignMA <designsma.contact@gmail.com>
2025-03-07 19:17:51 -0600

    "Terminando el algoritmo de dijkstra."

--- Python/Grafos/djikstra.py
+++ Python/Grafos/djikstra.py
@@ -1,90 +1,65 @@
 import networkx as nx 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 g=nx.DiGraph()#creamos el grafo
 
-print("Defina los vertices del grafo DIRIGIDO\n")
-while True:
-    vertice = input("\nIntroduce un vertice\n-1 para salir : ")
-    
-    if vertice == "-1":
-        print("\nSaliendo...")
-        break
-    
-    if not vertice.isalnum():
-        print("El vertice debe ser al menos un caracter o digito.")
-        continue
-    
-    if vertice not in g.nodes: # si el  vertice  no esta repetido
-        g.add_node(vertice) # añadir nodo
-    else:
-        print(f"El vertice '{vertice}' ya existe.")
+n_aristas=int(input("Ingrese el numero de aristas del grafo:"))#la base del programa es saber las aristas del grafo
 
-for vertice in g.nodes:
-    print(f"\nDefiniendo adyacencias para el vértice: '{vertice}'")
-    print("\nVértices disponibles:", g.nodes)
-    while True:
-        adj = input(f"\nIngresa un vértice adyacente a '{vertice}' (-1 para terminar): ").strip()  # Eliminar espacios en blanc
-        if adj == "-1":
-            break  # Salir del bucle para este vértice
-        
-        if adj in g.nodes:  # Verificar si el vértice adyacente existe
-            if not adj in g.adj[vertice]:  # Evitar duplicados
-                
-                while True:
-                    try:
-                        costo = input(f"\nDefine el costo del recorrido hacia {adj} | Solo positivos: ")
-                        costo = float(costo)
-                        if costo < 0:
-                            print("Solo se puede tener un costo mayor a cero.")
-                            continue # repetir
-                    except ValueError as e:
-                        print("No es un número real positivo.")
-                        continue
-                    
-                    break
-                
-                arista = (vertice,adj,costo)
-                g.add_edge(vertice,adj, weight=costo)
-                print(f"'\n{adj}' agregado como adyacente a '{vertice}' con un costo de {costo}.")
+#creacion de nodos y aristas para construir el grafo
+for _ in range(n_aristas):
+    nodo1=input("Ingrese la etiqueta del nodo: ")
+    nodo2=input("Ingrese la etiqueta del nodo: ")
+    peso=float(input("Ingrese el peso del camino/arista"))
+    g.add_edge(nodo1,nodo2,weight=peso)
 
-            else:
-                print(f"'{adj}' ya está en la lista de adyacencia de '{vertice}'.")
-        else:
-            print(f"Error: '{adj}' no es un vértice válido.")
+    
+#mostramos el grafo    
+print("El grafo construido es: ")
+print(g.nodes)
+print("Pesos de las aristas: ", g.edges(data=True))    
 
 
 # Dibujar el grafo
 ax = plt.subplot(111)  # Tamaño de la figura
 pos = nx.spring_layout(g)  # Posiciones de los nodos
 nx.draw(g, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=2000, font_size=10)
-
 # Dibujar los pesos en las aristas
 labels = nx.get_edge_attributes(g, 'weight')
 nx.draw_networkx_edge_labels(g, pos, edge_labels=labels)
-
-
-plt.show(block=False)#mostramos el grafo
+#mostramos el grafo
+plt.show(block=False)
 input("...")
 
 
-while True:
-    #ingrese el incio y el destino
-    inicio=input("Ingrese el nodo inicio: ")
-    destino=input("Ingrese el nodo final: ")
+#ingrese el incio y el destino
+inicio=input("ingrese el nodo inicio: ")
+destino=input("ingrese el nodo final: ")
 
 
-    #calculamos el camino ma corto con el método nx.shortest_path()
-    try:
-        camino_mas_corto=nx.shortest_path(g,inicio,destino,weight="weight")#algoritmo djiskstra inlcuido en la biblioteca networkx
-        print(f"\n Camino mas corto es: {camino_mas_corto}")
-    except nx.NetworkXNoPath:
-            print("no existe camino entre esos dos nodos")    
-    except nx.NodeNotFound:
-        print(f"nodo no encontrado")
-
+#calculamos el camino mas corto con el método nx.shortest_path()
+try:
+    camino_mas_corto=nx.shortest_path(g,inicio,destino,weight="weight")#algoritmo djiskstra inlcuido en la biblioteca networkx
+    print(f"\n Camino mas corto es: {camino_mas_corto}")
     
-            
-    
-        
+     # Extraer las aristas de la ruta más corta, se usa para mandarlo como parametro para dibujar
+    aristas_camino = list(zip(camino_mas_corto, camino_mas_corto[1:]))
 
+    # Dibujar el grafo con la ruta más corta resaltada,copie casi lo mismo de arriba solo que con el camino dibujado en rojo
+    ax = plt.subplot(111)  # Tamaño de la figura
+    pos = nx.spring_layout(g)  # Posiciones de los nodos
+    nx.draw(g, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=2000, font_size=10)
+    # Resaltar la ruta más corta en rojo
+    nx.draw_networkx_edges(g, pos, edgelist=aristas_camino, edge_color='red', width=2.5)
+    # Dibujar los pesos en las aristas
+    nx.draw_networkx_edge_labels(g, pos, edge_labels=labels)
+
+    plt.show(block=False)#mostramos el grafo
+    input("...")
+    
+    
+    
+    
+except nx.NetworkXNoPath:
+        print("no existe camino entre esos dos nodos")    
+except nx.NodeNotFound:
+    print(f"nodo no encontrado")
